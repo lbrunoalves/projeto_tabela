c11c2a7371c1c2e93b5732baeea7b9b094658e9d
lbrunoalves <139410779+lbrunoalves@users.noreply.github.com>
2024-01-31 20:11:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
 
-
-
 alunos = [
     {
         "matricula": 1,
         "nome": "Jonas Lopes",
         "idade": 18,
         "curso": "Javascript",
-        "novato": "Não"
+        "novato": False
     }
 ]
-
 matricula_atual = 1
 index = 0
 
+
 def atualizarTabela() -> None:
+    # Limpando a tabela
+    # get_children() -> Retorna as linhas da tabela
     for linha in tabela.get_children():
         tabela.delete(linha)
 
@@ -27,16 +27,15 @@ def atualizarTabela() -> None:
                                        aluno["idade"],
                                        aluno["curso"],
                                        aluno["novato"]))
+
+
 def adicionarAluno() -> None:
     global matricula_atual
     matricula_atual += 1
-    nome = texNome.get()
-    idade = int(texIdade.get())
+    nome = txtNome.get()
+    idade = int(txtIdade.get())
     curso = comboCursos.get()
-    if opcao.get() == True:
-        novato = "Sim"
-    else:
-        novato = "Não"
+    novato = opcao.get()
 
     aluno = {
         "matricula": matricula_atual,
@@ -47,77 +46,134 @@ def adicionarAluno() -> None:
     }
     messagebox.showinfo("Sucesso!", "Aluno adicionado com sucesso!")
     alunos.append(aluno)
+
+    # Limpar os campos
     limparCampos()
     atualizarTabela()
 
+
 def limparCampos() -> None:
-    texNome.delete(0, END)
-    texIdade.delete(0, END)
+    # Como habilitar o campo
+    txtMatricula.config(state=NORMAL)
+    txtMatricula.delete(0, END)
+    txtMatricula.config(state=DISABLED)
+    txtNome.delete(0, END)
+    txtIdade.delete(0, END)
     comboCursos.set("")
     opcao.set(False)
 
+
 def preencherCampos(event) -> None:
-    linha_selecionada = tabela.selection()
+    linha_selecionada = tabela.selection()  # I001
     global index
     index = tabela.index(linha_selecionada)
     aluno = alunos[index]
     limparCampos()
-    texMatricula.config(state=NORMAL)
-    texMatricula.insert(END, str(aluno["matricula"]))
-    texMatricula.config(state=DISABLED)
-    texNome.insert(END, aluno["nome"])
-    texIdade.insert(END, str(aluno["idade"]))
+    txtMatricula.config(state=NORMAL)
+    txtMatricula.insert(END, str(aluno['matricula']))
+    txtMatricula.config(state=DISABLED)
+    txtNome.insert(END, aluno["nome"])
+    txtNome.config(state=DISABLED)
+    txtIdade.insert(END, str(aluno["idade"]))
+    txtIdade.config(state=DISABLED)
     comboCursos.set(aluno["curso"])
+    comboCursos.config(state=DISABLED)
+
+def editarAluno() -> None:
+    opcaoSelecionada = messagebox.askyesno("Tem Certeza", "Deseja altera os dados?")
+    if opcaoSelecionada:
+        txtNome.config(state=NORMAL)
+        txtIdade.config(state=NORMAL)
+        comboCursos.config(state=NORMAL)
+        btnSalvar.config(state=NORMAL)
+def deletarAluno() -> None:
+    opcaoSelecionada = messagebox.askyesno("Tem certeza?", "Deseja remover o aluno?")
+    if opcaoSelecionada:
+        alunos.remove(alunos[index])
+        messagebox.showinfo("Sucesso!", "Dados apagados com sucesso!")
+    limparCampos()
+    atualizarTabela()
+
+def salvarAluno() -> None:
+    nome = txtNome.get()
+    idade = int(txtIdade.get())
+    curso = comboCursos.get()
+    novato = opcao.get()
+    opcaoSelecionada = messagebox.askyesno("Tem Certeza?", "Deseja altera os dados?")
+    if opcaoSelecionada:
+        aluno = alunos[index]
+        aluno['nome'] = nome
+        aluno['idade'] = idade
+        aluno['curso'] = curso
+        aluno['novato'] = novato
+        messagebox.showinfo("Sucesso", "Dados alterados com sucesso!")
+    limparCampos()
+    atualizarTabela()
 
 
 janela = Tk()
 
-janela.title("Alunos - infinity")
+janela.title("Alunos - Infinity")
 
+# janela.geometry("800x650")
 
-labelMatricula = Label(janela, text="Matricula:", font="Tahoma 18 bold", fg="red")
+labelMatricula = Label(janela, text="Matricula:", font="Tahoma 18 bold",
+                       fg="red")
 labelMatricula.grid(row=0, column=0)
+
+txtMatricula = Entry(janela, font="Tahoma 18", width=26,
+                     state=DISABLED)
+txtMatricula.grid(row=0, column=1)
+
 labelNome = Label(janela, text="Nome:", font="Tahoma 18 bold", fg="red")
 labelNome.grid(row=1, column=0)
+
+txtNome = Entry(janela, font="Tahoma 18", width=26)
+txtNome.grid(row=1, column=1)
+
 labelIdade = Label(janela, text="Idade:", font="Tahoma 18 bold", fg="red")
 labelIdade.grid(row=2, column=0)
+
+txtIdade = Entry(janela, font="Tahoma 18", width=26)
+txtIdade.grid(row=2, column=1)
+
 labelCurso = Label(janela, text="Curso:", font="Tahoma 18 bold", fg="red")
 labelCurso.grid(row=3, column=0)
-labelNovato = Label(janela, text="Novato?", font="Tahoma 18 bold", fg="red")
-labelNovato.grid(row=4, column=0)
-
-texMatricula = Entry(janela, font="Tahoma 18", width=26, state=DISABLED)
-texMatricula.grid(row=0, column=1)
-texNome = Entry(janela, font="Tahoma 18", width=26)
-texNome.grid(row=1, column=1)
-texIdade = Entry(janela, font="Tahoma 18", width=26)
-texIdade.grid(row=2, column=1)
-
 
 cursos = ["Javascript", "Python", "React", "NodeJs"]
-comboCursos = ttk.Combobox(janela, font="Tahoma 18", values=cursos, width=24)
+comboCursos = ttk.Combobox(janela, font="Tahoma 18", values=cursos,
+                           width=24)
 comboCursos.grid(row=3, column=1, sticky=W)
+
+labelNovato = Label(janela, text="Novato?", font="Tahoma 18 bold", fg="red")
+labelNovato.grid(row=4, column=0)
 
 opcao = BooleanVar(value=False)
 checkNovato = ttk.Checkbutton(janela, width=26, variable=opcao)
 checkNovato.grid(row=4, column=1, sticky=W)
 
-btnAdicionar = Button(janela, text="Adicionar", font="Tahoma 16", fg="red", height=1, command=adicionarAluno)
+btnAdicionar = Button(janela, text="Adicionar", font="Tahoma 16", fg="red",
+                      height=1, command=adicionarAluno)
 btnAdicionar.grid(row=5, column=0)
-btnEditar = Button(janela, text="Editar", font="Tahoma 16", fg="red", height=1)
+
+btnEditar = Button(janela, text="Editar", font="Tahoma 16", fg="red",
+                   height=1, command=editarAluno)
 btnEditar.grid(row=5, column=1)
-btnExcluir = Button(janela, text="Excluir", font="Tahoma 16", fg="red", height=1)
+
+btnExcluir = Button(janela, text="Excluir", font="Tahoma 16", fg="red",
+                    height=1, command=deletarAluno)
 btnExcluir.grid(row=5, column=2)
-btnPdf = Button(janela, text="Salvar PDF", font="Tahoma 16", fg="red", height=1)
-btnPdf.grid(row=7, column=1)
-btnImprimir = Button(janela, text="Imprimir", font="Tahoma 16", fg="red", height=1)
-btnImprimir.grid(row=7, column=2)
+
+btnSalvar = Button(janela, text="Salvar", font="Tahoma 16", fg="red",
+                    height=1, command=salvarAluno, state=DISABLED)
+btnSalvar.grid(row=4,column=2)
 
 colunas = ["Matricula", "Nome", "Idade", "Curso", "Novato"]
 tabela = ttk.Treeview(janela, columns=colunas, show="headings")
 for coluna in colunas:
     tabela.heading(coluna, text=coluna)
     tabela.column(coluna, width=110)
+
 tabela.grid(row=6, columnspan=3)
 tabela.bind("<ButtonRelease-1>", preencherCampos)
 
